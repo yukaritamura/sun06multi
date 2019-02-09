0994484d53a94c4db9c69893f713805d13a66b8b
Yukari Tamura <yukaritamura@outlook.jp>
2019-02-09 20:07:57 +0900

    Add blocken rom patches

---
+++ roms/blocken/make_ips.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+flips --create -i ic31j.bin ic31j.bin.patched ic31j.ips
+flips --create -i ic32j.bin ic32j.bin.patched ic32j.ips
---
+++ roms/blocken/patch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+flips --apply ic31j.ips ic31j.bin ic31j.bin.patched
+flips --apply ic32j.ips ic32j.bin ic32j.bin.patched
